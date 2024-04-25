7215b0de1e16eae9f9196872f8c40e071cf7701a
Marco <marcorosas1008@gmail.com>
2024-04-25 18:38:31 -0500

    PT consumo de 2 APIS

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`
+
+export function App () {
+
+    const [fact, setFact] = useState()
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then( data => setFact(data.fact))
+    }, [])
+
+
+    return (
+        <>
+            <h1>App de gatitos</h1>
+            {fact && <p>{fact}</p>}
+        </>
+    )
+}
