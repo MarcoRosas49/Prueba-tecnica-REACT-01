f4b0012fed7217c93bbb370b4bc84ad7f357cb45
Marco <marcorosas1008@gmail.com>
2024-04-25 20:57:44 -0500

    add separate useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import { useState } from "react"
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 //const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`
@@ -7,18 +8,39 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 export function App () {
 
     const [fact, setFact] = useState()
+    const [image, setImage] = useState()
 
+    //Para recuperar la palabra al cargar la página
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
         .then(res => res.json())
-        .then( data => setFact(data.fact))
+        .then( data => {
+            const {fact} = data
+            setFact(fact)
+        })
     }, [])
+
+    useEffect(() => {
+
+        if(!fact) return
+        const firstWord = fact.split(' ')[0]
+        
+        fetch(`https://cataas.com/cat/says/${firstWord}`)
+        .then(res => setImage(res.url))
+        
+    }, [fact])
+
 
 
     return (
-        <>
+        <main>
             <h1>App de gatitos</h1>
+
+            <section>
             {fact && <p>{fact}</p>}
-        </>
+            {image && <img src={image} alt={`Image using the first letter for ${fact}`} />}
+
+            </section>
+        </main>
     )
 }
